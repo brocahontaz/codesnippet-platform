b51a005e9b644927732c868b1cdb38a63884a69f
Johan Andersson <ja222um@student.lnu.se>
2020-02-24 13:02:56 +0100

    Added more needed packages and started on main app file

---
+++ app.js
@@ -0,0 +1,12 @@
+/** Main starting point of the application.
+ *
+ * @author Johan Andersson
+ * @version 1.0
+ */
+
+'use strict'
+
+const express = require('express')
+const handlebars = require('express-handlebars')
+
+const app = express()
