45869c8ac5a7113adebd1355f6cf17f565a5aa30
Johan Andersson <ja222um@student.lnu.se>
2020-02-24 13:18:37 +0100

    Started creating handlebars basis

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
 'use strict'
 
 const express = require('express')
-const handlebars = require('express-handlebars')
+const hbs = require('express-handlebars')
+const path = require('path')
+const logger = require('morgan')
 
 const app = express()
+
+// Set up view engine
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views', 'layouts,', 'default')
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
